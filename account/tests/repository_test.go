39a8663e2218b3033436d3cea76e7500eafcff98
Rauf Asadov <raufasadov23@gmail.com>
2026-06-24 12:54:51 +0400

    updated account tests

--- account/tests/repository_test.go
+++ account/tests/repository_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/rasadov/EcommerceAPI/account/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
