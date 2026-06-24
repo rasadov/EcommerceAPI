3324109e52f94147307042d7e490f6e8ecdc82d6
Rauf Asadov <raufasadov23@gmail.com>
2026-06-24 15:29:25 +0400

    new utility functions for kafka consumer

--- recommender/app/shared/kafka/utils.py
+++ recommender/app/shared/kafka/utils.py
@@ -6,3 +6,6 @@ def product_is_created_or_updated(event: dict) -> bool:
 
 def product_is_deleted(event: dict) -> bool:
     return event["type"] == EventType.PRODUCT_DELETED
+
+def is_interaction_event(event: dict) -> bool:
+    return event["type"] == EventType.INTERACTION_CREATED or event["type"] == EventType.INTERACTION_UPDATED
