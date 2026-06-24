6fe958218aec361975f05dd7f67c593b28da4d4b
Rauf Asadov <raufasadov23@gmail.com>
2026-06-24 23:24:17 +0400

    added nil kafka producer handling

--- pkg/kafka/producer.go
+++ pkg/kafka/producer.go
@@ -14,6 +14,12 @@ type ProducerService interface {
 }
 
 func SendMessageToRecommender(service ProducerService, event any, topic string) error {
+	producer := service.GetProducer()
+	if producer == nil {
+		log.Println("Kafka producer not configured, skipping message")
+		return nil
+	}
+
 	jsonMessage, err := json.Marshal(event)
 	if err != nil {
 		log.Println("Failed to marshal event:", err)
@@ -26,13 +32,18 @@ func SendMessageToRecommender(service ProducerService, event any, topic string) 
 	}
 
 	// Send the message asynchronously
-	service.GetProducer().Input() <- msg
+	producer.Input() <- msg
 
 	return nil
 }
 
 func CloseProducer(service ProducerService) {
-	if err := service.GetProducer().Close(); err != nil {
+	producer := service.GetProducer()
+	if producer == nil {
+		return
+	}
+
+	if err := producer.Close(); err != nil {
 		log.Printf("Failed to close producer: %v\n", err)
 	} else {
 		done <- true
