b61f973f35dee0251101ebb95149fef2c23e0ff1
Rauf Asadov <raufasadov23@gmail.com>
2026-06-26 14:08:01 +0400

    small fix in product service

--- product/models/product.go
+++ product/models/product.go
@@ -12,4 +12,5 @@ type ProductDocument struct {
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
+	AccountID   int     `json:"accountID"`
 }
