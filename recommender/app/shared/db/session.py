f80809895fd16212c5d3034931cb2178a20a8db2
Rauf Asadov <raufasadov23@gmail.com>
2026-07-12 21:42:42 +0400

    fix(recommender): Initialzie schema safely on startup

--- recommender/app/shared/db/session.py
+++ recommender/app/shared/db/session.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from shared.db.models import Base
 from shared.config.settings import DATABASE_URL
-
 
 replica_engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 ReplicaSession = sessionmaker(bind=replica_engine)
-Base.metadata.create_all(replica_engine)
+
 
 def get_session():
     return ReplicaSession()
